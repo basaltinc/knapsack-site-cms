69b8c575ecaceab1986cb5afbcce2c410ab91bf8
marywarrington <mary@basalt.io>
2018-11-13 11:15:39 -0800

    linting fix

--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,7 @@ const Header = () => (
     <Logo>
       <Link href="/">
         <a>Bedrock</a>
-        {/*<img src={'/../static/bedrock.svg'} />*/}
+        {/* <img src={'/../static/bedrock.svg'} /> */}
       </Link>
     </Logo>
     <Nav />
