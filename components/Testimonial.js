8195a3c44663ed339b0990e7de223096224b14c4
marywarrington <mary@basalt.io>
2018-12-06 10:17:39 -0800

    upate testimonials component to accept longer quotes

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -7,56 +7,37 @@ const TestimonialWrapper = styled.figure`
   overflow: hidden;
   margin: 0;
   width: 400px;
-  height: 200px;
+  display: flex;
+  min-height: 200px;
   text-align: left;
   box-shadow: ${props => props.theme.bs};
   font-size: 1.8rem;
-  background-color: ${props => props.theme.secondary};
+  background-color: #ffffff;
   figcaption {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    z-index: 1;
-    opacity: 1;
-    padding: 30px 0 30px 10px;
-    background-color: #ffffff;
-    width: 50%;
     display: flex;
     flex-direction: column;
     justify-content: center;
+    flex: 2 1 100%;
+    z-index: 1;
+    opacity: 1;
+    padding: 30px 10px;
     &:before {
       position: absolute;
       top: 50%;
       -webkit-transform: translateY(-50%);
       transform: translateY(-50%);
-      right: 100%;
+      right: 59%;
       content: '';
       width: 0;
       height: 0;
       border-style: solid;
-      border-width: 120px 120px 120px 0px;
+      border-width: 250px 105px 250px 0px;
       border-color: transparent #fff transparent transparent;
     }
-  }
-  &:after {
-    position: absolute;
-    bottom: 50%;
-    right: 40%;
-    content: '';
-    width: 0;
-    height: 0;
-    border-style: solid;
-    border-width: 110px 160px 0 120px;
-    border-color: ${props => props.theme.primary} transparent transparent
-      transparent;
   }
   h5,
   blockquote {
     line-height: 1.6em;
-    -webkit-transform: translateX(-30px);
-    transform: translateX(-30px);
-    margin: 0;
     color: ${props => props.theme.primary};
   }
   h5 {
@@ -70,21 +51,33 @@ const TestimonialWrapper = styled.figure`
   }
 `;
 
+const ImgWrapper = styled.div`
+  padding: 20px;
+  align-items: center;
+  flex: 1 1 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: linear-gradient(
+    180deg,
+    ${props => props.theme.secondary} 30%,
+    ${props => props.theme.secondaryExtra}
+  );
+`;
+
 const ProfilePicture = styled.img`
-  position: absolute;
   width: 110px;
   border-radius: 50%;
-  top: 50%;
-  left: -10px;
+  height: 110px;
   z-index: 2;
-  -webkit-transform: translate(50%, -50%);
-  transform: translate(50%, -50%);
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
 `;
 
 const Testimonial = props => (
   <TestimonialWrapper id="testimonials">
-    <ProfilePicture src={props.img} alt="profile-sample1" />
+    <ImgWrapper>
+      <ProfilePicture src={props.img} alt="profile-sample1" />
+    </ImgWrapper>
     <figcaption>
       <blockquote>{props.quote}</blockquote>
       <h5>- {props.name}</h5>
