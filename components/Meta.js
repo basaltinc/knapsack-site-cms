93a3cfe685aaefdafe3e7fd8074ba76a4fcc1117
marywarrington <mary@basalt.io>
2018-12-08 09:42:21 -0800

    add google console meta tag

--- components/Meta.js
+++ components/Meta.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
+    <meta name="google-site-verification" content="28IH24yr7RLWJjT3QiZrN-mUfFCKSUT653QXlPayiaY" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <meta
