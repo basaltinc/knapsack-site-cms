94c340ba04cdcc09c94823213f274092322b1831
Evan Lovely <EvanLovely@users.noreply.github.com>
2018-12-09 20:40:26 -0800

    doc changes (#48)
    * refactor(docs): use path param instead of query
    
    now every file in `./docs` will be available at `/docs/file-name` <- if it was `docs/file-name.md`
    
    * fix(docs): prevent short pages from hiding long sidebar

--- next.config.js
+++ next.config.js
@@ -3,29 +3,41 @@ const { join } = require('path');
 const md = require('marked');
 const matter = require('gray-matter');
 
-function sortDocs(a, b) {
-  if (a.data.order < b.data.order) return -1;
-  if (a.data.order > b.data.order) return 1;
-  return 0;
-}
+const docs = fs
+  .readdirSync(join(__dirname, 'docs'))
+  .filter(f => f.endsWith('md'))
+  .map(doc => {
+    const results = matter(
+      fs.readFileSync(join(__dirname, 'docs', doc)),
+      'utf8',
+    );
+    return {
+      ...results,
+      content: md.parse(results.content),
+      id: doc.replace('.md', ''),
+    };
+  });
 
 module.exports = {
   distDir: 'build',
   publicRuntimeConfig: {
-    docs: fs
-      .readdirSync(join(__dirname, 'docs'))
-      .filter(f => f.endsWith('md'))
-      .map(doc => {
-        const results = matter(
-          fs.readFileSync(join(__dirname, 'docs', doc)),
-          'utf8',
-        );
-        return {
-          ...results,
-          content: md.parse(results.content),
-          id: doc.replace('.md', ''),
-        };
-      })
-      .sort(sortDocs),
+    docs,
+  },
+  exportPathMap: defaultPathMap => {
+    const pathMap = {
+      ...defaultPathMap,
+    };
+
+    // now every file in `./docs` will be available at `/docs/file-name` <- if it was `docs/file-name.md`
+    docs.forEach(doc => {
+      pathMap[`/docs/${doc.id}`] = {
+        page: '/docs',
+        query: {
+          item: doc.id,
+        },
+      };
+    });
+
+    return pathMap;
   },
 };
